1c1c03202fefed91ed95130a777df7a612e84654
Svaboo <svabko28@gmail.com>
2025-03-27 11:25:43 +0100

    Changes to be committed: 	modified:   website/__pycache__/auth.cpython-313.pyc 	modified:   website/__pycache__/views.cpython-313.pyc 	modified:   website/auth.py 	modified:   website/templates/base.html 	new file:   website/templates/home.html 	modified:   website/views.py

--- website/views.py
+++ website/views.py
@@ -3,6 +3,5 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 @views.route('/')
-
 def home():
-    return render_template("home.html")
+    return render_template("base.html")
