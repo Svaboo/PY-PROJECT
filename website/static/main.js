4ce5033e858d048df72d02e733f058459210ff2f
Svaboo <svabko28@gmail.com>
2025-04-23 23:18:25 +0200

    modified:   website/static/main.js 	new file:   website/static/st.css 	modified:   website/templates/index.html

--- website/static/main.js
+++ website/static/main.js
@@ -1,49 +1,102 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
+const canvas = document.getElementById("experience-canvas");
+const sizes = {
+  width: window.innerWidth, 
+  height: window.innerHeight,
+};
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild(renderer.domElement);
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-directionalLight.position.set(5, 10, 7.5);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
-
-const gltfLoader = new GLTFLoader();
-const url = '/static/map.glb';
-
-gltfLoader.load(url, (gltf) => {
-    const root = gltf.scene;
-    console.log("Model naložen:", root);
-    root.position.set(0, 0, 0);
-    root.scale.set(1, 1, 1);
-    scene.add(root);
-}, undefined, (error) => {
-    console.error("Napaka pri nalaganju modela:", error);
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas, 
+  antialias: true,
 });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-camera.position.x = -6.440;
-camera.position.y = 17.907790044514503;
-camera.position.z = -34.640789754832838;
+renderer.shadowMap.enabled = true;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1.75;
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
+const loader = new GLTFLoader();
+loader.load(
+  "/map.glb",
+  function (glb) {
+    glb.scene.traverse((child) => {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+    scene.add(glb.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
-function animate() {
+const sun = new THREE.DirectionalLight(0xffffff);
+sun.castShadow = true;
+sun.position.set(75, 80, -30);
+sun.target.position.set(50, 0, 0);
+sun.shadow.mapSize.width = 4096;
+sun.shadow.mapSize.height = 4096;
+sun.shadow.camera.left = -100;
+sun.shadow.camera.right = 100;
+sun.shadow.camera.top = 100;
+sun.shadow.camera.bottom = -100;
+sun.shadow.normalBias = 0.2;
+scene.add(sun);
 
-    console.log(camera.position);
-    requestAnimationFrame(animate);
-    controls.update();
-    renderer.render(scene, camera);
+const shadowHelper = new THREE.CameraHelper(sun.shadow.camera);
+scene.add(shadowHelper); 
+console.log(sun.shadow);
+
+const helper = new THREE.DirectionalLightHelper(sun, 5);
+scene.add(helper);
+
+const light = new THREE.AmbientLight(0x404040, 3);
+scene.add(light);
+
+const aspect = sizes.width / sizes.height;
+const camera = new THREE.OrthographicCamera(
+  -aspect * 50, 
+  aspect * 50,
+  50,
+  -50,
+  1,
+  1000
+);
+camera.position.x = -13;
+camera.position.y = 39;
+camera.position.z = -67;
+
+const controls = new OrbitControls(camera, canvas);
+controls.update();
+
+function handleResize() {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  const aspect = sizes.width / sizes.height;
+  camera.left = -aspect * 50;
+  camera.right = aspect * 50;
+  camera.top = 50;
+  camera.bottom = -50;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 }
 
+window.addEventListener("resize", handleResize);
+
+function animate() {
+  renderer.render(scene, camera);
+  renderer.setAnimationLoop(animate);
+}
 animate();
