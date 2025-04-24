bef04c5062cd45ad3355d36aaeacea067da3c840
Svaboo <svabko28@gmail.com>
2025-04-24 11:42:29 +0200

    modified:   .DS_Store 	modified:   instance/database.db 	modified:   website/.DS_Store 	modified:   website/__pycache__/auth.cpython-313.pyc 	modified:   website/auth.py 	modified:   website/static/.DS_Store 	deleted:    website/static/main.js 	deleted:    website/static/map.glb 	deleted:    website/static/st.css 	modified:   website/templates/index.html

--- website/auth.py
+++ website/auth.py
@@ -66,7 +66,3 @@ def sign_up():
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
-
-@auth.route('/models')
-def model():
-    return render_template("index.html", user=current_user)
